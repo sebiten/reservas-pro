a725fd00130f3ff638c5d44a338c0dc36b18b09a
sebiten <sebdevspace@gmail.com>
2025-12-03 00:10:56 -0300

    suspense

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -1,10 +1,14 @@
 import { ForgotPasswordForm } from '@/components/forgot-password-form'
+import { Suspense } from 'react'
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
+        <Suspense>
+
         <ForgotPasswordForm />
+        </Suspense>
       </div>
     </div>
   )
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,10 +1,14 @@
 import { SignUpForm } from '@/components/sign-up-form'
+import { Suspense } from 'react'
 
 export default function Page() {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
+        <Suspense>
+
         <SignUpForm />
+        </Suspense>
       </div>
     </div>
   )
